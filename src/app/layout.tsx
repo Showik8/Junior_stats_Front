176e1066d3407b63bc19e2569f4d57d2495746fb
Showik8 <shotaabashidze88@gmail.com>
2026-03-07 13:39:59 +0400

    fix Hydratation:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ka" className={inter.variable}>
-      <body className={`${inter.className} antialiased`}>
+      <body suppressHydrationWarning className={`${inter.className} antialiased`}>
         <QueryProvider>
           {children}
         </QueryProvider>
